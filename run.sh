e980b4ee053b42ea201d301d431a38df0abbe6c3
Paulo Grolli Grabin <paulograbin@gmail.com>
2024-03-05 22:07:56 -0300

    Update run.sh with one option to print gc logs

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 echo "Run.sh..."
 
 if [ ! -f target/experiment-1.jar ]; then
-  echo "11111"
+  echo "Compiled program not found, cannot run anything..."
 else
-  echo "222"
-  java --class-path target/experiment-1.jar org.example.Main                                                                                                        130 ↵
+#  java -Xlog:gc --class-path target/experiment-1.jar org.example.Main
+  java --enable-preview --class-path target/experiment-1.jar org.example.Main
 fi
