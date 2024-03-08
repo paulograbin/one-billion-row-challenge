385387f9908549ded4f911623831533a0021017e
Paulo Grolli Grabin <paulograbin@gmail.com>
2024-03-08 19:48:58 -0300

    Use SDKMAN to set jvm

--- prepare.sh
+++ prepare.sh
@@ -2,6 +2,9 @@
 
 echo "Prepare.sh..."
 
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk use java 21.0.2-graal 1>&2
+
 if [ ! -f target/experiment-1.jar ]; then
   echo "Recompile..."
   mvn package
