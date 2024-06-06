1e866b29b864b056801edf0b4bc2a09c0449fc71
Paulo Grolli Grabin <paulograbin@gmail.com>
2024-06-06 16:24:58 -0300

    Add better run file

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/zsh
 
-echo "Run.sh..."
-
 if [ ! -f target/experiment-1.jar ]; then
   echo "Compiled program not found, cannot run anything..."
 else
-  java -Xlog:gc --enable-preview --class-path target/experiment-1.jar org.example.Main
+#  java -Xlog:gc --class-path target/experiment-1.jar org.example.Main
+  java -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC --enable-preview --class-path target/experiment-1.jar org.example.Main
 #  java --enable-preview --class-path target/experiment-1.jar org.example.Main
-fi
+
+
+
+
+time for i in {1..10}; do java -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC --enable-preview --class-path target/experiment-1.jar org.example.Main; done > native_no_ep.txt
+time for i in {1..10}; do java -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC --enable-preview --class-path target/experiment-1.jar org.example.Main; done > native_no_ep.txt
